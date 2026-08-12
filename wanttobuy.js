44d7a0627e1193d1bbf6a408dd20864f5fd09c3c
koraytugay <ktugay@sonatype.com>
2026-08-12 10:03:37 -0400

    Revert "Include all stores in store filter dropdown regardless of stock count"
    This reverts commit 698562df61c85fe44fb7d044073446f6146aa056.

--- wanttobuy.js
+++ wanttobuy.js
@@ -41,15 +41,25 @@ function populateStoreFilter() {
     const currentValue = storeSelect.value;
     storeSelect.innerHTML = '<option value="all">All Stores</option>';
 
+    const storesWithStock = [];
     STORES.forEach(store => {
         const inStockCount = allGames.filter(game => isGameInStockAtStore(game, store.key)).length;
+        if (inStockCount > 0) {
+            storesWithStock.push({
+                ...store,
+                count: inStockCount
+            });
+        }
+    });
+
+    storesWithStock.forEach(store => {
         const option = document.createElement('option');
         option.value = store.key;
-        option.textContent = `${store.name} (${inStockCount})`;
+        option.textContent = `${store.name} (${store.count})`;
         storeSelect.appendChild(option);
     });
 
-    if (STORES.some(s => s.key === currentValue)) {
+    if (storesWithStock.some(s => s.key === currentValue)) {
         storeSelect.value = currentValue;
     } else {
         storeSelect.value = 'all';
