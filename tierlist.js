a0e95c0266c74da06220f9801eec997fa5925f7b
Koray Tugay <ktugay@sonatype.com>
2026-08-21 13:37:39 -0400

    Update tierlist.js

--- tierlist.js
+++ tierlist.js
@@ -42,7 +42,7 @@ function renderTierList() {
         const rating = game.myRating;
         if (rating >= 10) {
             tiers.s.push(game);
-        } else if (rating >= 8) {
+        } else if (rating >= 9) {
             tiers.a.push(game);
         } else if (rating >= 7) {
             tiers.b.push(game);
